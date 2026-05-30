72cc199a40246ce7ebc0034281eeacde121fb0e5
BeGeiger <benedikt-geiger@gmx.de>
2026-05-30 12:30:17 -0400

    Port initial code from Salamander

---
+++ tests/test_klnmf.py
@@ -0,0 +1,91 @@
+import pickle
+
+import numpy as np
+import pandas as pd
+import pytest
+from anndata import AnnData
+
+from salamander.models import klnmf
+
+PATH = "tests/test_data"
+PATH_TEST_DATA = f"{PATH}/models/klnmf"
+
+
+@pytest.fixture
+def adata():
+    counts = pd.read_csv(f"{PATH_TEST_DATA}/counts.csv", index_col=0)
+    adata = AnnData(counts.T)
+    return adata
+
+
+@pytest.fixture(params=[1, 2])
+def n_signatures(request):
+    return request.param
+
+
+@pytest.fixture
+def W_init(n_signatures):
+    return np.load(f"{PATH_TEST_DATA}/W_init_nsigs{n_signatures}.npy")
+
+
+@pytest.fixture
+def asignatures_init(W_init, adata):
+    asignatures = AnnData(W_init.T)
+    asignatures.var_names = adata.var_names
+    return asignatures
+
+
+@pytest.fixture
+def H_init(n_signatures):
+    return np.load(f"{PATH_TEST_DATA}/H_init_nsigs{n_signatures}.npy")
+
+
+@pytest.fixture
+def model_init(adata, asignatures_init, H_init):
+    n_signatures = asignatures_init.n_obs
+    model = klnmf.KLNMF(n_signatures=n_signatures)
+    model.adata = adata
+    model.asignatures = asignatures_init
+    model.adata.obsm["exposures"] = H_init.T
+    return model
+
+
+@pytest.fixture
+def objective_init(n_signatures):
+    return np.load(f"{PATH_TEST_DATA}/objective_init_nsigs{n_signatures}.npy")
+
+
+def test_objective_function(model_init, objective_init):
+    assert np.allclose(model_init.objective_function(), objective_init)
+
+
+class TestUpdatesKLNMF:
+    @pytest.fixture
+    def WH_updated(self, n_signatures):
+        with open(
+            f"{PATH_TEST_DATA}/WH_updated_joint_nsigs{n_signatures}.pkl", "rb"
+        ) as f:
+            WH_updated = pickle.load(f)
+        return WH_updated
+
+    def test_update_parameters(self, model_init, WH_updated):
+        model_init._update_parameters()
+        W_updated, H_updated = WH_updated
+        assert np.allclose(model_init.asignatures.X, W_updated.T)
+        assert np.allclose(model_init.adata.obsm["exposures"], H_updated.T)
+
+    def test_given_signatures(self, n_signatures, adata):
+        for n_given_signatures in range(1, n_signatures + 1):
+            given_asignatures = adata[:n_given_signatures, :].copy()
+            given_asignatures.X = given_asignatures.X / np.sum(
+                given_asignatures.X, axis=1, keepdims=True
+            )
+            model = klnmf.KLNMF(
+                n_signatures=n_signatures,
+                min_iterations=3,
+                max_iterations=3,
+            )
+            model.fit(adata, given_parameters={"asignatures": given_asignatures})
+            assert np.allclose(
+                given_asignatures.X, model.asignatures.X[:n_given_signatures, :]
+            )
---
+++ tests/test_plot.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+from salamander import plot
+
+
+@pytest.fixture
+def data():
+    counts = np.array([[1, 1], [2, 3], [3, 2], [4, 4]])
+    sample_names = ["a", "b", "c", "d"]
+    data = pd.DataFrame(counts, index=sample_names)
+    return data
+
+
+def test_get_obs_order_normalized(data):
+    obs_order = plot.get_obs_order(data, normalize=True)
+
+    # A next to D
+    position_a = np.where(obs_order == "a")[0][0]
+    position_d = np.where(obs_order == "d")[0][0]
+    assert np.abs(position_a - position_d) == 1
+
+    # B as far away from C as possible
+    position_b = np.where(obs_order == "b")[0][0]
+    position_c = np.where(obs_order == "c")[0][0]
+    assert np.abs(position_b - position_c) == 3
+
+
+def test_get_obs_order_unnormalized(data):
+    obs_order = plot.get_obs_order(data, normalize=False)
+
+    # A as far away from D as possible
+    position_a = np.where(obs_order == "a")[0][0]
+    position_d = np.where(obs_order == "d")[0][0]
+    assert np.abs(position_a - position_d) == 3
+
+    # B next to C
+    position_b = np.where(obs_order == "b")[0][0]
+    position_c = np.where(obs_order == "c")[0][0]
+    assert np.abs(position_b - position_c) == 1
+
+
+def test_reorder_data(data):
+    # reordering is based on the relative values
+    data_reordered = plot._reorder_data(data)
+    obs_order = data_reordered.index.to_numpy()
+
+    # A next to D
+    position_a = np.where(obs_order == "a")[0][0]
+    position_d = np.where(obs_order == "d")[0][0]
+    assert np.abs(position_a - position_d) == 1
+
+    # B as far away from C as possible
+    position_b = np.where(obs_order == "b")[0][0]
+    position_c = np.where(obs_order == "c")[0][0]
+    assert np.abs(position_b - position_c) == 3
+
+
+def test_reorder_data_custom(data):
+    custom_obs_order = ["b", "a", "c", "d"]
+    data_reordered = plot._reorder_data(data, obs_order=custom_obs_order)
+    assert np.array_equal(data_reordered.index, custom_obs_order)
